31d214dfd42e6a991415adc1e14d991546bc1efe
berschirker <43097841+berschirker@users.noreply.github.com>
2019-12-18 23:27:16 -0500

    wee!

--- CHATWINDOW/index.php
+++ CHATWINDOW/index.php
@@ -99,7 +99,7 @@ function loadLog(){
         success: function(html){
             $("#chatbox").html(html); //Insert chat log into the #chatbox div
 
-            //Auto-scroll
+            //Aato-scroll
             var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
             if(newscrollHeight > oldscrollHeight){
                 $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
